31af093309d0d95858a87e611f00348195597fba
Dmitry Dudin <ddudin@harlemnext.com>
2023-01-03 22:18:25 +0200

    feat: reset config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  routeRules: {
-    "/": { static: true },
-  },
+  // routeRules: {
+  //   "/": { static: true },
+  // },
   // nitro: {
   //   prerender: {
   //     routes: ["/"],
