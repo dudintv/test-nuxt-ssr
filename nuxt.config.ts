2d6f2b57310d267efa257c936dd4f7a588783f68
Dmitry Dudin <ddudin@harlemnext.com>
2023-01-03 22:05:41 +0200

    feat: make config as default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
-  routeRules: {
-    "/": { prerender: true },
-  },
-  nitro: {
-    prerender: {
-      crawlLinks: false,
-      routes: ["/"],
-    },
-  },
+  // routeRules: {
+  //   "/": { prerender: true },
+  // },
+  // nitro: {
+  //   prerender: {
+  //     crawlLinks: false,
+  //     routes: ["/"],
+  //   },
+  // },
 });
